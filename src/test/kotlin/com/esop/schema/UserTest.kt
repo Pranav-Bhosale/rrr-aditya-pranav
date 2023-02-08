e51da0aa697c54f8c665cc5d6650700f99466a7d
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-02-08 14:57:16 +0530

    Format and remove warnings from Test files

--- src/test/kotlin/com/esop/schema/UserTest.kt
+++ src/test/kotlin/com/esop/schema/UserTest.kt
@@ -1,7 +1,6 @@
 package com.esop.schema
 
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -35,6 +34,9 @@ class UserTest {
         userOne.transferLockedESOPsTo(userTwo, EsopTransferRequest("PERFORMANCE", currentTradeQuantity))
 
         Assertions.assertEquals(expectedFreeInventoryForUserOne, userOne.userPerformanceInventory.getFreeInventory())
-        Assertions.assertEquals(expectedNonPerformanceInventoryForUserTwo, userTwo.userNonPerfInventory.getFreeInventory())
+        Assertions.assertEquals(
+            expectedNonPerformanceInventoryForUserTwo,
+            userTwo.userNonPerfInventory.getFreeInventory()
+        )
     }
 }
